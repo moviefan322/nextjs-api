950173520eeddebba3703877e8155d889f554ecc
moviefan322 <philman202@yahoo.com>
2023-06-06 15:09:06 -0400

    loads props dynamically

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -21,6 +21,17 @@ function SingleProduct(props: Props) {
   );
 }
 
+async function getData() {
+  const filePath: string = path.join(
+    process.cwd(),
+    "data",
+    "dummy-backend.json"
+  );
+  const jsonData: string = await fs.readFileSync(filePath, "utf-8");
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export async function getStaticProps(context: any) {
   // fetch data for a single product
   const { params } = context;
@@ -34,7 +45,7 @@ export async function getStaticProps(context: any) {
     "dummy-backend.json"
   );
   const jsonData: string = await fs.readFileSync(filePath, "utf-8");
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find(
     (product: any) => product.id === productId
@@ -48,15 +59,15 @@ export async function getStaticProps(context: any) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product: any) => product.id);
+
+  const params = ids.map((id: any) => ({ params: { pid: id } }));
   // fallback: false means that "not found" pages will be resolved into 404 pages
   // fallback: true means that "not found" pages will be resolved into a fallback page
   // fallback: "blocking" means that "not found" pages will be resolved into a fallback page, but only after the data is loaded
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
+    paths: params,
     fallback: false,
   };
 }
